250023948a3c5dc811e9744c50dbee6d4775aaa3
Saeedovic <Seedxlss@gmail.com>
2023-02-28 19:26:54 +0400

    fixed the obstacle generator

--- Assets/Saeed/ObstacleGenerator.cs
+++ Assets/Saeed/ObstacleGenerator.cs
@@ -4,36 +4,52 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+   private List<GameObject> obstaclePrefabs;
 
-    public float spawnInterval = 2f;
+   private int maxObjects = 5;
+   private float spawnHeight = 1f;
+   private float space = 1f;
 
-    public float spawnWidth = 10f;
-    public float spawnDepth = 10f;
+    private List<GameObject> obstacles = new List<GameObject>();
 
-    public float spawnHeight = 1f;
-
-
-    public int maxObstacles = 5;
-
-    public List<GameObject> obstacles = new List<GameObject>();
-
-    
-    void Start()
+    private void Start()
     {
-        StartCoroutine(SpawnObstacles());
+        SpawnObstacles();
     }
-    IEnumerator SpawnObstacles()
+
+    private void SpawnObstacles()
     {
-        while (obstacles.Count < maxObstacles)
+        for (int i = 0; i < maxObjects; i++)
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnWidth / 2f, spawnWidth / 2f), spawnHeight, Random.Range(-spawnDepth / 2f, spawnDepth / 2f));
+            
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)];
+            Vector3 spawnPos = GetRandomSpawnPosition();
             GameObject newObstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.identity);
 
             obstacles.Add(newObstacle);
-            Debug.Log("ADDED OBJECTTTTT");
-
-            yield return new WaitForSeconds(spawnInterval);
         }
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        Vector3 spawnPos = Vector3.zero;
+
+        bool Emptyplace = false;
+        while (!Emptyplace)
+        {
+           
+            spawnPos = new Vector3(Random.Range(transform.position.x - transform.localScale.x / 2f, transform.position.x + transform.localScale.x / 2f),  spawnHeight,Random.Range(transform.position.z - transform.localScale.z / 2f, transform.position.z + transform.localScale.z / 2f));
+            Emptyplace = true;
+            foreach (GameObject obstacle in obstacles)
+            {
+                if (Vector3.Distance(spawnPos, obstacle.transform.position) < space)
+                {
+                    Emptyplace = false;
+                    break;
+                }
+            }
+        }
+
+        return spawnPos;
     }
 }
